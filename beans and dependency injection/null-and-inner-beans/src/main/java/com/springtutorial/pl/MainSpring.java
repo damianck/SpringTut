f0a7cee04a9c5e7efb2ad5b9ea4e38197a526459
Daman Wtorek <damian.wtr@gmail.com>
2017-07-20 16:05:22 +0200

    null and inner beans

---
+++ beans and dependency injection/null-and-inner-beans/src/main/java/com/springtutorial/pl/MainSpring.java
@@ -0,0 +1,26 @@
+package com.springtutorial.pl;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainSpring {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		User user = context.getBean("user",User.class);
+
+		System.out.println(user.toString());
+		
+		User user2 = context.getBean("user2",User.class);
+
+		System.out.println(user2.toString());
+		
+	}
+
+}
