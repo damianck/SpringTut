bba607b24d792b8568cb5bb5b7242c17de15c025
Daman Wtorek <damian.wtr@gmail.com>
2017-07-20 18:19:53 +0200

    autowired by name

---
+++ beans and dependency injection/autowired-by-name/src/main/java/com/springtutorial/pl/InjectedBean.java
@@ -0,0 +1,14 @@
+package com.springtutorial.pl;
+
+public class InjectedBean {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
