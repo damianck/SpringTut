8261bc7b1b1b5a5fdb15b76ccfd8e40e9a26439c
Daman Wtorek <damian.wtr@gmail.com>
2017-07-21 12:33:56 +0200

    annotation config - component scan xml

---
+++ beans and dependency injection/annotation-config/src/main/java/com/springtutorial/pl/implementation/LoggerImpl.java
@@ -0,0 +1,14 @@
+package com.springtutorial.pl.implementation;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.springtutorial.pl.api.Logger;
+
+@Service
+public class LoggerImpl implements Logger {
+	public void log(String message) {
+		System.out.println(new Date() + ": " + message);
+	}
+}
---
+++ beans and dependency injection/annotation-config/src/main/java/com/springtutorial/pl/implementation/UsersRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.springtutorial.pl.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springtutorial.pl.api.Logger;
+import com.springtutorial.pl.api.UsersRepository;
+import com.springtutorial.pl.domain.User;
+
+@Service
+public class UsersRepositoryImpl implements UsersRepository {
+	@Autowired
+	private Logger logger;
+
+	public User createUser(String name) {
+		logger.log("Creating user " + name);
+		return new User(name);
+	}
+
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+}
