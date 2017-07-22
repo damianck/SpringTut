20cba8ea3ca2fbc5a775b2a6aefb28ebf5ee4c7b
Daman Wtorek <damian.wtr@gmail.com>
2017-07-22 12:38:37 +0200

    @ComponentScan using

---
+++ beans and dependency injection/component-scan-java/src/main/java/com/springtutorial/pl/AppConfiguration.java
@@ -0,0 +1,27 @@
+package com.springtutorial.pl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.springtutorial.pl.api.Logger;
+import com.springtutorial.pl.api.UsersRepository;
+import com.springtutorial.pl.implementation.DateLogger;
+import com.springtutorial.pl.implementation.UsersRepositoryImpl;
+
+@Configuration
+@ComponentScan(basePackages = "com.springtutorial.pl")
+public class AppConfiguration {
+
+	// @Bean
+	// public Logger logger() {
+	// return new DateLogger();
+	// }
+	//
+	// @Bean
+	// public UsersRepository usersRepository() {
+	// UsersRepositoryImpl usersRepository = new UsersRepositoryImpl();
+	// usersRepository.setLogger(logger());
+	// return usersRepository;
+	// }
+}
