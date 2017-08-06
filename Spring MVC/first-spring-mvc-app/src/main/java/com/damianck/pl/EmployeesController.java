e495a6f27bd65b8c10bd6896a61163331b7c56cf
Damian Wtorek <damian.wtr@gmail.com>
2017-08-06 18:19:30 +0200

    first spring mvc application

---
+++ Spring MVC/first-spring-mvc-app/src/main/java/com/damianck/pl/EmployeesController.java
@@ -0,0 +1,22 @@
+package com.damianck.pl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class EmployeesController {
+    @RequestMapping("getEmployees")
+    public List<Empoloyee> getEmployee() {
+        List<Empoloyee> result = new ArrayList<>();
+
+        result.add(new Empoloyee("John", "Butters", new BigDecimal("1234.56")));
+        result.add(new Empoloyee("Steve", "Clussy", new BigDecimal("5223.12")));
+        result.add(new Empoloyee("Anne", "Cliston", new BigDecimal("3654.34")));
+
+        return result;
+    }
+}
---
+++ Spring MVC/first-spring-mvc-app/src/main/java/com/damianck/pl/FirstSpringMvcAppApplication.java
@@ -0,0 +1,12 @@
+package com.damianck.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringMvcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringMvcAppApplication.class, args);
+	}
+}
