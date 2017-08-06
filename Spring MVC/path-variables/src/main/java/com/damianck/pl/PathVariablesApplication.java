e6e05ba28164d2bcf9a63d87fd02a9e97d8c9f0e
Damian Wtorek <damian.wtr@gmail.com>
2017-08-06 19:29:56 +0200

    path variable

---
+++ Spring MVC/path-variables/src/main/java/com/damianck/pl/PathVariablesApplication.java
@@ -0,0 +1,12 @@
+package com.damianck.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathVariablesApplication.class, args);
+	}
+}
