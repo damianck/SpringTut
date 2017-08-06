65f528c7b723bf7cb2c88412006fecf8c7617a79
Damian Wtorek <damian.wtr@gmail.com>
2017-08-06 20:52:20 +0200

    regex

---
+++ Spring MVC/regex-in-paths/src/main/java/com/damianck/pl/RegexInPathsApplication.java
@@ -0,0 +1,12 @@
+package com.damianck.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexInPathsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexInPathsApplication.class, args);
+	}
+}
