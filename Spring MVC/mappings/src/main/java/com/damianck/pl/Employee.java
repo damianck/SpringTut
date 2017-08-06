424ba82b89a32e5b442773fb066fe18295b0d490
Damian Wtorek <damian.wtr@gmail.com>
2017-08-06 18:55:58 +0200

    mapping - methods get and post

---
+++ Spring MVC/mappings/src/main/java/com/damianck/pl/Employee.java
@@ -0,0 +1,39 @@
+package com.damianck.pl;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private BigDecimal salary;
+
+    public Employee(String name, String surname, BigDecimal salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
